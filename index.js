cb2b3cc1cabea2f1a03f04f04826c4ab80a152a8
Aniket Prajapati <prajapati.ani306@gmail.com>
2019-02-01 13:19:48 +0530

    Image to Matrix

---
+++ index.js
@@ -0,0 +1,9 @@
+let photoReader = require('./photoReader');
+
+photoReader.read('sampleImage.png', function name(error, pixels, info) {
+    if (error) {
+        console.log('error reading file');
+        return;
+    }
+    console.log(pixels[13][19]);
+})
