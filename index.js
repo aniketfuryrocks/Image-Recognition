b679b30ff84be6220a0566e9190436490c7e48ec
MrAniketDev <prajapati.ani306@gmail.com>
2019-03-03 15:26:35 +0530

    s

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ photoReader.read('sampleImage.png', function name(error, pixels, info) {
         console.log('error reading file');
         return;
     }
-    console.log(pixels[13][19]);
     let grayScale = photoReader.getGrayScale(pixels);
 })
